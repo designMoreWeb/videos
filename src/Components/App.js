daf7d02b29853e2975d9c391f96bf4f72afec558
Divy Tolia <divystolia@gmail.com>
2019-01-14 16:14:29 -0500

    conditional rendering

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,10 +4,10 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 import VideoList from'./VideoList';
-
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component{
-    state={videos:[],selectedVideo:null};
+    state={videos:[],selectedVideo:[]};
     onFormSubmit= async (term) => {
         const response = await youtube.get('/search',{
             params:{
@@ -19,15 +19,19 @@ class App extends React.Component{
     };
 
     onVideoSelect = (video) => {
-        console.log('From the App!',video);
+       this.setState({selectedVideo:video});
     };
 
     render(){
        return(
            <div className='ui container'>
             <SearchBar onFormSubmit={this.onFormSubmit}/>
-            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
-           </div>
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList 
+                onVideoSelect={this.onVideoSelect} 
+                videos={this.state.videos}
+            />
+            </div>
        )
     }
 };
--- src/Components/VideoDetail.js
+++ src/Components/VideoDetail.js
@@ -1,2 +1,11 @@
 //React files needed
 import React from 'react';
+
+//User created components
+
+
+const VideoDetail = (props) =>{
+    return <div>{video.snippet.title}</div>;
+};
+
+export default VideoDetail;
