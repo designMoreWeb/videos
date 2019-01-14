f6fa3d6625b9bd3babf2d32583891b77de4908d1
designMoreWeb <divystolia@gmail.com>
2019-01-14 16:42:26 -0500

    removed duplicate alt props

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -7,8 +7,11 @@ import '../css/VideoItem.css';
 const VideoItem = ({video, onVideoSelect}) =>{
     return (
             <div onClick={() => onVideoSelect(video) } className="video-item item">
-                <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} 
-                alt={video.snippet.title} />
+                <img 
+                    alt={video.snippet.title} 
+                    className="ui image" 
+                    src={video.snippet.thumbnails.medium.url}
+                />
                 <div className='content'>
                     <div className='header'>{video.snippet.title}</div>
                 </div>
