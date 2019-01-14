82c2ea3bf5ac0160f248cfc51d07be198b69495d
designMoreWeb <divystolia@gmail.com>
2019-01-14 15:44:59 -0500

    changed className on img tags

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -7,7 +7,8 @@ import './VideoItem.css';
 const VideoItem = ({video}) =>{
     return (
             <div className="video-item item">
-                <img className="ui image" src={video.snippet.thumbnails.medium.url}/>
+                <img className="ui image" src={video.snippet.thumbnails.medium.url} 
+                alt={video.snippet.title} />
                 <div className='content'>
                     <div className='header'>{video.snippet.title}</div>
                 </div>
