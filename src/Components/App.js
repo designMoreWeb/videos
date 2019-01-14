5e47a454d0c4549367314c6cbb6495f9eccd0c10
designMoreWeb <divystolia@gmail.com>
2019-01-14 11:56:30 -0500

    new app created new files

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+//React files needed
+import React from 'react';
+
+//User created components
+import SearchBar from './SearchBar';
+import { ReactComponent } from '*.svg';
+
+class App extends ReactComponent{
+    
+};
+
+export default App;
