cacf6c6d40af82fceb84e77da9c66dc789123612
designMoreWeb <divystolia@gmail.com>
2019-01-14 12:31:15 -0500

    prevent default behaviour

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -8,10 +8,18 @@ class SearchBar extends React.Component{
         this.setState({term:event.target.value});
     };
 
+    onFormSubmit = event =>{
+        //prevent page from refreshing 
+        event.preventDefault();
+        
+        //callback from parent component
+
+    };
+
     render(){
         return(
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
                         <input 
