d9d2ae6ecfaff1ab25bb1c636793d688db939cdc
designMoreWeb <divystolia@gmail.com>
2019-01-14 14:51:09 -0500

    rendering video thumbnails

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -1,8 +1,13 @@
 //React files needed
 import React from 'react';
 
-const VideoItem = (props) =>{
-    return <div>Video Item</div>
+const VideoItem = ({video}) =>{
+    return (
+            <div>
+            <img src={video.snippet.thumbnails.medium.url}/>
+                {video.snippet.title}
+            </div>
+        );
 };
 
 export default VideoItem;
--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -5,7 +5,7 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos }) =>{
     const renderVideoList = videos.map((video) => {
-        return <VideoItem />;
+        return <VideoItem video={video} />;
     });
     return<div>{renderVideoList}</div>;
 };
