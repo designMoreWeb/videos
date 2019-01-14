7ba7438a96caa7e3962ba9b5f9a65254051a7659
designMoreWeb <divystolia@gmail.com>
2019-01-14 12:50:28 -0500

    config youtube axios

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,5 +1,14 @@
 import Axios from 'axios';
 
-export default Axios.create({
-    const KEY = 'AIzaSyABZ5d4N-MQMrytTgYKPY-gEu83tSLRDCA';
+export default 
+    Axios.create({
+        baseURL: 'https://www.googleapis.com/youtube/v3',
+        params:{
+            part:'snippet',
+            maxResults:5,
+            key:KEY
+        },
+        headers:{
+            Authorization:'Client-ID AIzaSyABZ5d4N-MQMrytTgYKPY-gEu83tSLRDCA'
+        }
 });
