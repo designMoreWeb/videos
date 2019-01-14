3a445dfa7e0597b41f79f9d32de4e26c75275c02
Divy Tolia <divystolia@gmail.com>
2019-01-14 16:44:49 -0500

    defaulting video selection

--- src/Components/App.js
+++ src/Components/App.js
@@ -16,7 +16,10 @@ class App extends React.Component{
             }
         });
 
-        this.setState({videos:response.data.items});
+        this.setState({
+            videos:response.data.items,
+            selectedVideo:response.data.items[0]
+        });
     };
 
     onVideoSelect = (video) => {
