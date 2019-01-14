29bda33f4dffa933a6b4be23640effcf0c9c4f70
Divy Tolia <divystolia@gmail.com>
2019-01-14 16:26:20 -0500

    displaying video player

--- src/Components/VideoDetail.js
+++ src/Components/VideoDetail.js
@@ -8,12 +8,17 @@ const VideoDetail = ({video}) =>{
     if (!video){
         return <div>Loading...</div>
     }
+
+    const videoSrc=`https://www.youtube.com/embed/${video.id.videoId}`;
     return(
+        <div className = "ui embed">
+            <iframe src = {videoSrc} />
+        </div>
          <div className="ui segment">
             <h4 className="ui header">{video.snippet.title}</h4>
             <p>{video.snippet.description}</p>
         </div>
-         );
+    );   
 };
 
 export default VideoDetail;
